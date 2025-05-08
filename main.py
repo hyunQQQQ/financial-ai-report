0c25f48ac06c9de1708136ead58403238863e77b
최현규 <hyunq9504@gmail.com>
2025-05-08 16:11:21 +0900

    price API 구현 및 data 출력 테스트

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
+import requests
 from fastapi import FastAPI
 
 app = FastAPI()
 
+def get_bitcoin_price():
+    url = "https://api.upbit.com/v1/ticker?markets=KRW-BTC"
+    response = requests.get(url)
+    data = response.json()
+    ###print(data)
+    return {
+        "price": data[0]["trade_price"],
+        "high": data[0]["high_price"],
+        "low": data[0]["low_price"],
+        "change_rate": data[0]["signed_change_rate"]
+    }
+
 @app.get("/")
 def read_root():
     return {"message": "Hello, FastAPI!"}
+
+@app.get("/price")
+def price():
+    return get_bitcoin_price()
